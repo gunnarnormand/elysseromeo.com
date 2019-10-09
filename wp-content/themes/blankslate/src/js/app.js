09dbaa5dea3834119ff9f55b6dbd276e1359d94f
Gunnar Normand <gunnar@gunnar.tech>
2019-10-08 23:18:21 -0400

    setup cursor/canvas

--- wp-content/themes/blankslate/src/js/app.js
+++ wp-content/themes/blankslate/src/js/app.js
@@ -33,6 +33,7 @@ const app = (function () {
   const $links = document.querySelectorAll('a');
   const $aboutPageLinks = document.querySelectorAll('a.link');
 	const innerCursor = document.querySelector(".cursor--small");
+	const canvas = document.querySelector(".cursor--canvas");
 
   const loaderModule = () => {
     const $footerNav = document.querySelector('.onepage-pagination');
@@ -117,29 +118,80 @@ const app = (function () {
 
 		let clientX = -100;
 		let clientY = -100;
-
 		const initCursor = () => {
-
 			document.addEventListener("mousemove", e => {
 		    clientX = e.clientX;
 		    clientY = e.clientY;
 		  });
-
 		  const render = () => {
-
 		    TweenMax.set(innerCursor, {
 		      x: clientX,
 		      y: clientY
 		    });
-
-		    requestAnimationFrame(render);
+		    window.requestAnimationFrame(render);
 		  };
-
-		  requestAnimationFrame(render);
-
+		  window.requestAnimationFrame(render);
 		};
 
 		initCursor();
+
+		let lastX = 0;
+		let lastY = 0;
+		let isStuck = false;
+		let showCursor = false;
+		let group;
+		let stuckX;
+		let stuckY;
+		let fillOuterCursor;
+		const initCanvas = () => {
+			const shapeBounds = {
+				width: 75,
+				height: 75,
+			};
+			paper.setup(canvas);
+			const strokeColor = 'rgba(60, 74, 83, 0.5)';
+			const strokeWidth = 1;
+			const segments = 8;
+			const radius = 15;
+
+			const noiseScale = 150;
+			const noiseRange = 4;
+			let isNoisy = false;
+
+			const polygon = new paper.Path.RegularPolygon(
+				new paper.Point(0,0),
+				segments,
+				radius,
+			);
+			polygon.strokeColor = strokeColor;
+  		polygon.strokeWidth = strokeWidth;
+  		polygon.smooth();
+  		group = new paper.Group([polygon]);
+  		group.applyMatrix = false;
+
+			const noiseObjects = polygon.segments.map(() => new SimplexNoise());
+  		let bigCoordinates = [];
+
+			const lerp = (a, b, n) => {
+				return (1 - n) * a + n * b;
+			};
+
+			const map = (value, in_min, in_max, out_min, out_max) => {
+				return (
+					((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+				);
+			};
+
+			paper.view.onFrame = event => {
+				lastX = lerp(lastX, clientX, 0.2);
+				lastY = lerp(lastY, clientY, 0.2);
+				group.position = new paper.Point(lastX, lastY);
+			}
+		}
+
+		initCanvas();
+
+
 
 
 
