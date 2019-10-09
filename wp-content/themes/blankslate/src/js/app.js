c0f94aa3347f3f20dee5d6975a05bc36c482da21
Gunnar Normand <gunnar@gunnar.tech>
2019-10-08 23:51:09 -0400

    complete paper.view.onFrame

--- wp-content/themes/blankslate/src/js/app.js
+++ wp-content/themes/blankslate/src/js/app.js
@@ -176,27 +176,93 @@ const app = (function () {
 				);
 			};
 			paper.view.onFrame = event => {
-				lastX = lerp(lastX, clientX, 0.2);
-				lastY = lerp(lastY, clientY, 0.2);
-				group.position = new paper.Point(lastX, lastY);
+
+
+				if (!isStuck) {
+			    // move circle around normally
+			    lastX = lerp(lastX, clientX, 0.2);
+			    lastY = lerp(lastY, clientY, 0.2);
+			    group.position = new paper.Point(lastX, lastY);
+			  } else if (isStuck) {
+			    // fixed position on a nav item
+			    lastX = lerp(lastX, stuckX, 0.2);
+			    lastY = lerp(lastY, stuckY, 0.2);
+			    group.position = new paper.Point(lastX, lastY);
+			  }
+
+				if (isStuck && polygon.bounds.width < shapeBounds.width) {
+					// scale up the shape
+					polygon.scale(1.15);
+				} else if (!isStuck && polygon.bounds.width > 30) {
+					// remove noise
+					if (isNoisy) {
+					  polygon.segments.forEach((segment, i) => {
+					    segment.point.set(bigCoordinates[i][0], bigCoordinates[i][1]);
+					  });
+					  isNoisy = false;
+					  bigCoordinates = [];
+					}
+					// scale down the shape
+					const scaleDown = 0.92;
+					polygon.scale(scaleDown);
+				}
+
+				// while stuck and big, apply simplex noise
+			  if (isStuck && polygon.bounds.width >= shapeBounds.width) {
+			    isNoisy = true;
+			    // first get coordinates of large circle
+			    if (bigCoordinates.length === 0) {
+			      polygon.segments.forEach((segment, i) => {
+			        bigCoordinates[i] = [segment.point.x, segment.point.y];
+			      });
+			    }
+
+			    // loop over all points of the polygon
+			    polygon.segments.forEach((segment, i) => {
+
+			      // get new noise value
+			      // we divide event.count by noiseScale to get a very smooth value
+			      const noiseX = noiseObjects[i].noise2D(event.count / noiseScale, 0);
+			      const noiseY = noiseObjects[i].noise2D(event.count / noiseScale, 1);
+
+			      // map the noise value to our defined range
+			      const distortionX = map(noiseX, -1, 1, -noiseRange, noiseRange);
+			      const distortionY = map(noiseY, -1, 1, -noiseRange, noiseRange);
+
+			      // apply distortion to coordinates
+			      const newX = bigCoordinates[i][0] + distortionX;
+			      const newY = bigCoordinates[i][1] + distortionY;
+
+			      // set new (noisy) coodrindate of point
+			      segment.point.set(newX, newY);
+			    });
+
+			  }
+			  polygon.smooth();
+
+
+
 			}
 		}
 		initCanvas();
 
 		const initCursorHovers = () => {
-			const handleCursorMouseEnter = e => {
+			const handleBasicCursorMouseEnter = e => {
 				const navItem = e.currentTarget;
 				const navItemBox = navItem.getBoundingClientRect();
 				stuckX = Math.round(navItemBox.left + navItemBox.width / 2);
 				stuckY = Math.round(navItemBox.top + navItemBox.height / 2);
 				isStuck = true;
 			};
-			const handleCursorMouseLeave = () => {
+			const handleBasicCursorMouseLeave = () => {
 				isStuck = false;
 			};
-			$links.forEach(link => {
-				link.addEventListener("mouseenter", handleCursorMouseEnter);
-				link.addEventListener("mouseleave", handleCursorMouseLeave);
+			$aboutLink.addEventListener("mouseenter", handleBasicCursorMouseEnter);
+			$aboutLink.addEventListener("mouseleave", handleBasicCursorMouseLeave);
+			const $paginationLinks = document.querySelectorAll('.onepage-pagination li a');
+			$paginationLinks.forEach(link => {
+				link.addEventListener("mouseenter", handleBasicCursorMouseEnter);
+				link.addEventListener("mouseleave", handleBasicCursorMouseLeave);
 			});
 		}
 		initCursorHovers();
